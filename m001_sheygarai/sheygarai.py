30f6608ab1bdb3fa1b7cdb37453133c7fd6cc689
Mohsen Javidpanah <moshen212@gmail.com>
2024-02-13 23:19:06 +0330

    Improved the code and header description basically added

--- m001_sheygarai/sheygarai.py
+++ m001_sheygarai/sheygarai.py
@@ -1,3 +1,8 @@
+'''
+# To Do: Insert here some words about this project
+'''
+
+
 class daneshjo:
     def __int__(self, fname, lname, field, code, grade):
         self.fname = fname
@@ -5,17 +10,21 @@ class daneshjo:
         self.field = field
         self.code = code
         self.grade = grade
+
     def set_info(self, fname, lname, field, code, grade):
         self.fname = fname
         self.lname = lname
         self.field = field
         self.code = code
         self.grade = grade
+
     def readALL(self):
         self.fname = input("name khodra vared konid:")
         self.lname = input("name khanevadegi:")
         self.field = input("reshte:")
         self.code = int(input("shomare daneshjoi:"))
         self.grade = int(input("term chand:"))
+
     def __str__(self):
         return
+
