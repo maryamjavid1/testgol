0dba8029801306dc202a086db4d05c2ad820fc6e
maryamjavid1 <156774559+maryamjavid1@users.noreply.github.com>
2024-02-13 12:19:14 +0330

    Update sheygarai.py
    the solution of objectism

--- m001_sheygarai/sheygarai.py
+++ m001_sheygarai/sheygarai.py
@@ -1,1 +1,21 @@
-class
+class daneshjo:
+    def __int__(self, fname, lname, field, code, grade):
+        self.fname = fname
+        self.lname = lname
+        self.field = field
+        self.code = code
+        self.grade = grade
+    def set_info(self, fname, lname, field, code, grade):
+        self.fname = fname
+        self.lname = lname
+        self.field = field
+        self.code = code
+        self.grade = grade
+    def readALL(self):
+        self.fname = input("name khodra vared konid:")
+        self.lname = input("name khanevadegi:")
+        self.field = input("reshte:")
+        self.code = int(input("shomare daneshjoi:"))
+        self.grade = int(input("term chand:"))
+    def __str__(self):
+        return
